5f90564b6933c047d9f14d82643c1fb7b29ecebe
nero58 <nero58.deepak@gmail.com>
2023-08-12 12:13:33 +0530

    blog

---
+++ blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+
+posts = [
+    {
+        'author': 'Core',
+        'title':'Blog Postl',
+        'content': 'First Post',
+        'date_posted':'24tj Ausgust 2025'
+    },
+    {
+        'author': 'joe',
+        'title':'Blog Pastfsd',
+        'content': 'second Post',
+        'date_posted':'25th Ausgust 2025'
+    }
+]
+
+
+def home(request):
+    context={
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context,)
+
+
+def about(request):
+    return render(request,'blog/about.html',{'title': 'About'})
+
