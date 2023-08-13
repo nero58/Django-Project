3321fc8d316b07cf5abdf576395d06e2b9917c1d
nero58 <nero58.deepak@gmail.com>
2023-08-13 20:08:57 +0530

    new changes

--- blog/views.py
+++ blog/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render
 from .models import Post
 
 
-posts = [
-    {
-        'author': 'Core',
-        'title':'Blog Postl',
-        'content': 'First Post',
-        'date_posted':'24tj Ausgust 2025'
-    },
-    {
-        'author': 'joe',
-        'title':'Blog Pastfsd',
-        'content': 'second Post',
-        'date_posted':'25th Ausgust 2025'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Core',
+#         'title':'Blog Postl',
+#         'content': 'First Post',
+#         'date_posted':'24tj Ausgust 2025'
+#     },
+#     {
+#         'author': 'joe',
+#         'title':'Blog Pastfsd',
+#         'content': 'second Post',
+#         'date_posted':'25th Ausgust 2025'
+#     }
+# ]
 
 
 def home(request):
